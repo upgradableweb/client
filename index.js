69b2dfb24aca9f695518108c048dc4268fc9d1b7
bharathps123 <b1997oct@gmail.com>
2024-01-06 20:51:58 +0530

    added new response catch

--- index.js
+++ index.js
@@ -3,9 +3,18 @@
  */
 
 /**
+ * @typedef {object} RequestOptions
+ * @property {object} [query] - Query parameters for the request.
+ * @property {string} [authorization] - Authorization token for the request.
+ * @property {() => void} [setLoading] - Function to set loading state.
+ * @property {() => void} [onResponse] - Function to handle successful response.
+ * @property {() => void} [onError] - Function to handle errors.
+ */
+
+/**
  * @param {string} url - The URL to send the request to.
  * @param {object | string} body - The body to send the request to.
- * @param {{ query?: object, authorization?: string }} options - Other options for the request.
+ * @param {RequestOptions} options - handling options for the request.
  * @returns {Promise} - A promise that resolves with the response data.
  */
 
@@ -18,7 +27,7 @@ export async function POST(url, body, options) {
 /**
  * @param {string} url - The URL to send the request to.
  * @param {object | string} body - The body to send the request to.
- * @param {{ query?: object, authorization?: string }} options - Other options for the request.
+ * @param {RequestOptions} options - handling options for the request.
  * @returns {Promise} - A promise that resolves with the response data.
  */
 
@@ -31,7 +40,7 @@ export async function PUT(url, body, options) {
 /**
  * @param {string} url - The URL to send the request to.
  * @param {object | string} body - The body to send the request to.
- * @param {{ query?: object, authorization?: string }} options - Other options for the request.
+ * @param {RequestOptions} options - handling options for the request.
  * @returns {Promise} - A promise that resolves with the response data.
  */
 
@@ -44,7 +53,7 @@ export async function DELETE(url, body, options) {
 /**
  * @param {string} url - The URL to send the request to.
  * @param {object | string} body - The body to send the request to.
- * @param {{ query?: object, authorization?: string }} options - Other options for the request.
+ * @param {RequestOptions} options - handling options for the request.
  * @returns {Promise} - A promise that resolves with the response data.
  */
 
@@ -57,11 +66,11 @@ export async function PATCH(url, body, options) {
 /**
  * @param {string} url - The URL to send the request to.
  * @param {object | string} body - The body to send the request to.
- * @param {{ query?: object, authorization?: string }} options - Other options for the request.
+ * @param {RequestOptions} options - handling options for the request.
  * @returns {Promise} - A promise that resolves with the response data.
  */
 
-export async function GET(url, body, options = { query, authorization }) {
+export async function GET(url, body, options) {
     let ops = options || {}
     ops.method = 'GET'
     return await FETCH(url, body, ops)
@@ -74,7 +83,7 @@ export async function GET(url, body, options = { query, authorization }) {
  */
 export const fetchConfig = { baseUrl: '', headers: { authorization: '' } }
 
-async function FETCH(url, body, { method, query, authorization }) {
+async function FETCH(url, body, { method, query, authorization, setLoading, onResponse, onError }) {
 
     if (fetchConfig.baseUrl && !url.startsWith('http')) {
         url = fetchConfig.baseUrl + url
@@ -92,19 +101,33 @@ async function FETCH(url, body, { method, query, authorization }) {
     if (authorization) {
         hs.authorization = authorization
     }
-
-    let res = await fetch(url, { body, method, headers: hs })
-    let data = await res.json()
-    data.responseStatus = res.status
-    data.responseText = res.statusText
-    if (!res.ok) {
-        if (!data.message) {
-            data.message = res.statusText
+    try {
+        setLoading && setLoading(true)
+        let res = await fetch(url, { body, method, headers: hs })
+        let data = await res.json()
+        data.responseStatus = res.status
+        data.responseText = res.statusText
+        if (!res.ok) {
+            if (!data.message) {
+                data.message = res.statusText
+            }
+            throw data
         }
-        throw data
-    }
 
-    return data
+        if (onResponse) {
+            onResponse(data)
+        } else {
+            return data
+        }
+
+    } catch (error) {
+        onError && onError(error)
+        if (!onResponse) {
+            throw error
+        }
+    } finally {
+        setLoading && setLoading(false)
+    }
 }
 
 export function Params(params = {}) {
